cefb5a44dc961bf0aad1d896e5075a30c97c7b1d
GAO Z <jiagao.zheng@gmail.com>
2018-11-13 22:07:53 -0800

    update UI

--- static/main.js
+++ static/main.js
@@ -18,40 +18,42 @@ $(document)
         $loading.hide();
     });
 
+var veh_no_prefix = 'xxx-xxx-x';
+
 //form related
-var default_input_csv_string = 'Veh_No,SOB,MC101,MC201,MC301,RF101,RF201,RF301,FF101,UB101,BS101,CL100,CL101,CL102,BS201,FI101,FO101,LS101,A metric,B metric,CL201,AS101,BS Audit,C metric\n' +
-    '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '2,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0\n' +
-    '3,8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '4,10,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0\n' +
-    '5,11,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1\n' +
-    '6,12,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '7,13,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '8,14,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '9,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '10,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '11,17,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '12,18,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\n' +
-    '13,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '14,20,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '15,21,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\n' +
-    '16,22,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '17,23,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1\n' +
-    '18,24,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '19,25,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0\n' +
-    '20,26,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '21,27,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '22,28,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0\n' +
-    '23,29,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\n' +
-    '24,30,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0\n' +
-    '25,31,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0';
+var default_input_csv_string = 'Veh_No,FirstDay,BIWSOB,MC101,MC201,MC301,RF101,RF201,RF301,FF101,UB101,BS101,CL100,CL101,CL102,BS201,FI101,FO101,LS101,A metric,B metric,CL201,AS101,BS Audit,C metric,TRANSSOB,TRANS1,PAINT,TRANS2,GASOB,T1,T2,WT001,T3,T4,T5,C1,C2,C3,C4,PTI,DR,NF001,DF001,FI001,DC001,LV001,SI001,VFC,ATF,QC001,DVT,QC002,QC003,QC004,CAMO,BLT001,BLT002,WT002,WT003,D metric,RQA,GCA,GLD001,VehicleEOB,PACK1\n' +
+    '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '2,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '3,8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '4,10,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '5,11,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0\n' +
+    '6,12,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,1,0,1,1,0\n' +
+    '7,13,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '8,14,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,1,0,1,1,1\n' +
+    '9,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '10,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '11,17,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '12,18,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '13,19,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '14,20,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '15,21,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0\n' +
+    '16,22,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '17,23,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0\n' +
+    '18,24,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,1\n' +
+    '19,25,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,1\n' +
+    '20,26,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,1\n' +
+    '21,27,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,1,0,1,1,1\n' +
+    '22,28,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '23,29,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0\n' +
+    '24,30,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0\n' +
+    '25,31,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0';
 
 var input_json = Papa.parse(default_input_csv_string);
 console.log(input_json);
 
 var veh_list = $('#form_container #veh_list');
 var veh_item_template = '<tr data-id="{0}">\n' +
-    '            <th scope="row">xxx-xxx-xx{1}</th>\n' +
+    '            <th scope="row">{1}</th>\n' +
     '            <td>{2}</td>\n' +
     '            <td>{3}</td>\n' +
     '            <td>\n' +
@@ -63,7 +65,7 @@ var veh_item_template = '<tr data-id="{0}">\n' +
 render_veh_list(input_json);
 function render_veh_list(json) {
     for(var i = 1; i < json.data.length; i++) {
-        veh_list.append(String.format(veh_item_template, json.data[i][0], json.data[i][0], json.data[i][1], json.data[i].join('-')));
+        veh_list.append(String.format(veh_item_template, json.data[i][0], veh_no_prefix + json.data[i][0], json.data[i][1], json.data[i].join('-')));
     }
     bind_edit_del_events();
 }
@@ -95,79 +97,6 @@ function get_click_item_index(element) {
 //gantt chart related
 var gantt_initilaized = false;
 
-var demo_tasks = {
-    "data":[
-        {"id":11, "text":"Project #1", "start_date":"28-03-2018", "duration":"11", "progress": 0.6, "open": true},
-        {"id":1, "text":"Project #2", "start_date":"01-04-2018", "duration":"18", "progress": 0.4, "open": true},
-        {"id":2, "text":"Task #1", "start_date":"02-04-2018", "duration":"8", "parent":"1", "progress":0.5, "open": true},
-        {"id":3, "text":"Task #2", "start_date":"11-04-2018", "duration":"8", "parent":"1", "progress": 0.6, "open": true},
-        {"id":4, "text":"Task #3", "start_date":"13-04-2018", "duration":"6", "parent":"1", "progress": 0.5, "open": true},
-        {"id":5, "text":"Task #1.1", "start_date":"02-04-2018", "duration":"7", "parent":"2", "progress": 0.6, "open": true},
-        {"id":6, "text":"Task #1.2", "start_date":"03-04-2018", "duration":"7", "parent":"2", "progress": 0.6, "open": true},
-        {"id":7, "text":"Task #2.1", "start_date":"11-04-2018", "duration":"8", "parent":"3", "progress": 0.6, "open": true},
-        {"id":8, "text":"Task #3.1", "start_date":"14-04-2018", "duration":"5", "parent":"4", "progress": 0.5, "open": true},
-        {"id":9, "text":"Task #3.2", "start_date":"14-04-2018", "duration":"4", "parent":"4", "progress": 0.5, "open": true},
-        {"id":10, "text":"Task #3.3", "start_date":"14-04-2018", "duration":"3", "parent":"4", "progress": 0.5, "open": true},
-
-        {"id":12, "text":"Task #1", "start_date":"03-04-2018", "duration":"5", "parent":"11", "progress": 1, "open": true},
-        {"id":13, "text":"Task #2", "start_date":"02-04-2018", "duration":"7", "parent":"11", "progress": 0.5, "open": true},
-        {"id":14, "text":"Task #3", "start_date":"02-04-2018", "duration":"6", "parent":"11", "progress": 0.8, "open": true},
-        {"id":15, "text":"Task #4", "start_date":"02-04-2018", "duration":"5", "parent":"11", "progress": 0.2, "open": true},
-        {"id":16, "text":"Task #5", "start_date":"02-04-2018", "duration":"7", "parent":"11", "progress": 0, "open": true},
-
-        {"id":17, "text":"Task #2.1", "start_date":"03-04-2018", "duration":"2", "parent":"13", "progress": 1, "open": true},
-        {"id":18, "text":"Task #2.2", "start_date":"06-04-2018", "duration":"3", "parent":"13", "progress": 0.8, "open": true},
-        {"id":19, "text":"Task #2.3", "start_date":"10-04-2018", "duration":"4", "parent":"13", "progress": 0.2, "open": true},
-        {"id":20, "text":"Task #2.4", "start_date":"10-04-2018", "duration":"4", "parent":"13", "progress": 0, "open": true},
-        {"id":21, "text":"Task #4.1", "start_date":"03-04-2018", "duration":"4", "parent":"15", "progress": 0.5, "open": true},
-        {"id":22, "text":"Task #4.2", "start_date":"03-04-2018", "duration":"4", "parent":"15", "progress": 0.1, "open": true},
-        {"id":23, "text":"Task #4.3", "start_date":"03-04-2018", "duration":"5", "parent":"15", "progress": 0, "open": true}
-    ],
-    "links":[
-        {"id":"1","source":"1","target":"2","type":"1"},
-        {"id":"2","source":"2","target":"3","type":"0"},
-        {"id":"3","source":"3","target":"4","type":"0"},
-        {"id":"4","source":"2","target":"5","type":"2"},
-        {"id":"5","source":"2","target":"6","type":"2"},
-        {"id":"6","source":"3","target":"7","type":"2"},
-        {"id":"7","source":"4","target":"8","type":"2"},
-        {"id":"8","source":"4","target":"9","type":"2"},
-        {"id":"9","source":"4","target":"10","type":"2"},
-        {"id":"10","source":"11","target":"12","type":"1"},
-        {"id":"11","source":"11","target":"13","type":"1"},
-        {"id":"12","source":"11","target":"14","type":"1"},
-        {"id":"13","source":"11","target":"15","type":"1"},
-        {"id":"14","source":"11","target":"16","type":"1"},
-        {"id":"15","source":"13","target":"17","type":"1"},
-        {"id":"16","source":"17","target":"18","type":"0"},
-        {"id":"17","source":"18","target":"19","type":"0"},
-        {"id":"18","source":"19","target":"20","type":"0"},
-        {"id":"19","source":"15","target":"21","type":"2"},
-        {"id":"20","source":"15","target":"22","type":"2"},
-        {"id":"21","source":"15","target":"23","type":"2"}
-    ]
-};
-
-var demo_tasks2 = {
-    "data":[
-        {"id":11, "text":"Project #1", "start_date":"28-03-2018", "duration":"11", "progress": 0.6, "open": true},
-        {"id":1, "text":"Project #2", "start_date":"01-04-2018", "duration":"18", "progress": 0.4, "open": true},
-        {"id":2, "text":"Task #1", "start_date":"02-04-2018", "duration":"8", "parent":"1", "progress":0.5, "open": true},
-        {"id":3, "text":"Task #2", "start_date":"11-04-2018", "duration":"8", "parent":"1", "progress": 0.6, "open": true},
-        {"id":4, "text":"Task #3", "start_date":"13-04-2018", "duration":"6", "parent":"1", "progress": 0.5, "open": true},
-        {"id":5, "text":"Task #1.1", "start_date":"02-04-2018", "duration":"7", "parent":"2", "progress": 0.6, "open": true},
-        {"id":6, "text":"Task #1.2", "start_date":"03-04-2018", "duration":"7", "parent":"2", "progress": 0.6, "open": true},
-        {"id":7, "text":"Task #2.1", "start_date":"11-04-2018", "duration":"8", "parent":"3", "progress": 0.6, "open": true},
-        {"id":8, "text":"Task #3.1", "start_date":"14-04-2018", "duration":"5", "parent":"4", "progress": 0.5, "open": true},
-        {"id":9, "text":"Task #3.2", "start_date":"14-04-2018", "duration":"4", "parent":"4", "progress": 0.5, "open": true},
-        {"id":10, "text":"Task #3.3", "start_date":"14-04-2018", "duration":"3", "parent":"4", "progress": 0.5, "open": true},
-    ],
-    "links":[
-    ]
-};
-
-var i = 0;
-
 function init() {
     $('#submit').click(function () {
         console.log('submit');
@@ -177,13 +106,11 @@ function init() {
         $.post('/scheduler', {csv: submit_csv})
             .done(function (data) {
                 console.log("Data Loaded: " + data);
+                var gantt_data = parse_csv_data(data);
                 $('.loading_modal').hide();
                 $('#form_container').hide();
                 $('#gantt_container').show();
-                //demo
-                var data = i > 0 ? demo_tasks : demo_tasks2;
-                draw_gantt(data);
-                i++;
+                draw_gantt(gantt_data);
             });
     });
     $('#gantt_container .back').click(function () {
@@ -191,6 +118,34 @@ function init() {
         $('#gantt_container').hide();
         $('#form_container').show();
     });
+}
+
+function parse_csv_data(data) {
+    var json = Papa.parse(data);
+    var tasks = {
+        "data":[],
+        "links":[]
+    };
+    for(var i = 1; i < json.data.length; i++) {
+        var row = json.data[i];
+        if (row && row.length > 1) {
+            var id = veh_no_prefix + row[0];
+            var proj = {"id": id, "text": id, "open": true};
+            tasks.data.push(proj);
+            for (var j = 1; j < row.length; j++) {
+                var task_name = json.data[0][j];
+                var content = row[j].split('$');
+                if (content[0] != -1) {
+                    //2018-10-15 08:30:00
+                    var task = {"id": id + task_name, "text": task_name, "start_date": content[0], "duration": content[1], "parent": id};
+                    // var task = {"id": id + task_name, "text": task_name, "start_date": "01-04-2018 08:00", "duration": content[1], "parent": id, "progress":0.5, "open": true};
+                    tasks.data.push(task);
+                }
+            }
+        }
+    }
+    console.log(tasks);
+    return tasks;
 }
 
 function draw_gantt(data) {
@@ -203,22 +158,26 @@ function gantt_init() {
         return;
     }
     gantt.config.readonly = true;
-    gantt.config.xml_date = "%d-%m-%Y %H:%i";
+    gantt.config.xml_date = "%Y-%m-%d %H:%i:%s";
     gantt.config.scale_unit = "hour";
     gantt.config.step = 1;
     gantt.config.date_scale = "%g %a";
     gantt.config.min_column_width = 20;
     gantt.config.duration_unit = "minute";
-    gantt.config.duration_step = 60;
+    gantt.config.duration_step = 1;
     gantt.config.scale_height = 75;
+    // gantt.config.row_height = 22;
+    gantt.config.static_background = true;
     gantt.config.subscales = [
         {unit: "day", step: 1, date: "%j %F, %l"},
         {unit: "minute", step: 15, date: "%i"}
     ];
     gantt.config.columns = [
         {name: "text", tree: true, width: '*', resize: true},
-        {name: "start_date", align: "center", resize: true},
-        {name: "duration", align: "center"}
+        {name: "start_date", align: "left", width: 100, resize: true, template:function(obj){
+                return moment(obj.start_date).format('YY-MM-DD hh:mm');
+        }},
+        {name: "duration", align: "right"}
     ];
     gantt.templates.scale_cell_class = function (date) {
         if (date.getDay() == 0 || date.getDay() == 6) {
@@ -229,6 +188,19 @@ function gantt_init() {
         if (date.getDay() == 0 || date.getDay() == 6) {
             return "weekend";
         }
+    };
+    gantt.templates.tooltip_text = function(start,end,task){
+        return "<b>Task:</b> "+task.text+"<br/><b>Start time:</b> " +
+            moment(start).format('YYYY-MM-DD hh:mm')+
+            "<br/><b>End time:</b> "+moment(end).format('YYYY-MM-DD hh:mm');
+    };
+    gantt.ignore_time = function (date) {
+        if (date.getDay() == 0 || date.getDay() == 6)
+            return true;
+        if (date.getHours() < 8 || date.getHours() > 18)
+            return true;
+
+        return false;
     };
     gantt.init("gantt");
     gantt_initilaized = true;
