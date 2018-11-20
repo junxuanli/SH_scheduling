1ade2ab2a123794b55e8d2355523b7e33275d2c3
GAO Z <jiagao.zheng@gmail.com>
2018-11-19 22:16:30 -0800

    fix datetime parser and update app logging

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ def index():
 @app.route('/scheduler', methods=['POST'])
 def scheduler():
     dt_str = request.form['dt']
-    dt = datetime.strptime(dt_str, '%m/%d/%Y %I:%M')
+    dt = datetime.strptime(dt_str, '%m/%d/%Y %H:%M')
     csv = StringIO(request.form['csv'])
     if csv:
         # return scheduling.main(csv)
@@ -29,11 +29,11 @@ def internal_error(exception):
 if __name__ == "__main__":
     if app.debug is not True:
         import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('flask.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-    file_handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    app.logger.addHandler(file_handler)
+        from logging.handlers import RotatingFileHandler
+        file_handler = RotatingFileHandler('flask.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+        file_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        file_handler.setFormatter(formatter)
+        app.logger.addHandler(file_handler)
     app.run(host='0.0.0.0', port=80)
     # app.run()
